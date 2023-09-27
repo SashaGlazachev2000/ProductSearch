e985acc80f3a6f3c7bae3fc5a9f87992fbcec161
SashaGlazachev2000 <70701239+SashaGlazachev2000@users.noreply.github.com>
2023-09-27 15:06:07 +0300

    ProductSearch
    Test

---
+++ ProductSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProductSearch
+//
+//  Created by Alexandr Glazachev on 27.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //  Hello
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
