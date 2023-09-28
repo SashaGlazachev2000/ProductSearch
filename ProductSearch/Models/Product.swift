15124512c11e69fc8cde235d06479ef3d39dbe3f
SashaGlazachev2000 <70701239+SashaGlazachev2000@users.noreply.github.com>
2023-09-28 15:13:27 +0300

    feat: New essence Product

--- ProductSearch/Models/Product.swift
+++ ProductSearch/Models/Product.swift
@@ -1,1 +1,12 @@
 import Foundation
+
+struct Product {
+    let image: String
+    let code: Int
+    let barcode: Int
+    let name: String
+    let amount: Int
+    let price: Float
+    let notSeasonal: Bool
+    let quality: [String]
+}
